1157f8bd2c6f1165e4c1e888e10ab26e20f85511
robhyrkiel <rob.hyrkiel@gmail.com>
2019-06-18 14:51:47 -0400

    chore: added single, page, header, and footer file templates

---
+++ themes/fultonU/index.php
@@ -0,0 +1,13 @@
+<h1><?php bloginfo('name')?></h1>
+<p><?php bloginfo('description')?></p>
+
+<?php 
+    while(have_posts() ) :
+        the_post(); ?>
+            <h2><a href="<?php the_permalink()?>"><?php the_title();?></a></h2>
+            <?php the_content(); ?>
+            <hr>
+        <?php
+    endwhile ;
+?>
+
