9f52ccbd9b2c6295934813e48dfb0689956f42fa
robhyrkiel <rob.hyrkiel@gmail.com>
2019-06-19 10:33:12 -0400

    feat: added dynamic title tags

--- themes/fultonU/functions.php
+++ themes/fultonU/functions.php
@@ -1,9 +1,9 @@
 <?php 
 
 function uni_files() {
-    wp_enqueue_script('main_uni_js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+    wp_enqueue_script('main_uni_js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
     
-    wp_enqueue_style('uni_main_styles', get_stylesheet_uri());
+    wp_enqueue_style('uni_main_styles', get_stylesheet_uri(), NULL, microtime());
     wp_enqueue_style('font-awesome', "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
     wp_enqueue_style('google-fonts', "https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
 
@@ -11,4 +11,10 @@ function uni_files() {
 
 add_action('wp_enqueue_scripts', 'uni_files');
 
+function uni_features() {
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'uni_features')
+;
 ?>
