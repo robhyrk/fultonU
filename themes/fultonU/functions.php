ffcf58ba1220cfbb75d59f6ac2455a23e8a74cbf
Rob H <robh@Mac-mini.local>
2019-08-02 03:18:51 -0400

    test: adding mu-plugins to functions.php

--- themes/fultonU/functions.php
+++ themes/fultonU/functions.php
@@ -50,6 +50,59 @@ function uni_adjust_queries($query) {
     }
 }
 
-add_action('pre_get_posts', 'uni_adjust_queries')
+add_action('pre_get_posts', 'uni_adjust_queries');
+
+function uni_post_types() {
+    //Event pOSt Type
+    register_post_type('event', array(
+        'has_archive' => true,
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor', 'excerpt'),
+        'rewrite' => array('slug' => 'events'),
+        'public' => true,
+        'labels' => array(
+            'name' => 'Events',
+            'add_new_item' => 'Add New Event',
+            'edit_item' => 'Edit Event',
+            'all_items' => 'All Events',
+            'singular_name' => 'Event'
+        ),
+        'menu_icon' => 'dashicons-calendar'
+    ));
+
+    //Program Post Type
+    register_post_type('program', array(
+        'has_archive' => true,
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor'),
+        'rewrite' => array('slug' => 'programs'),
+        'public' => true,
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program'
+        ),
+        'menu_icon' => 'dashicons-awards'
+    ));
+
+    // Post Type
+    register_post_type('instructor', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor', 'thumbnail'),
+        'public' => true,
+        'labels' => array(
+            'name' => 'Instructors',
+            'add_new_item' => 'Add New Instructor',
+            'edit_item' => 'Edit Instructor',
+            'all_items' => 'All Instructors',
+            'singular_name' => 'Instructor'
+        ),
+        'menu_icon' => 'dashicons-welcome-learn-more'
+    ));
+}
+
+add_action('init', 'uni_post_types');
 
 ?>
