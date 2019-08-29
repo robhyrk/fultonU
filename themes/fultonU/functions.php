7a5df3c09ecf19095136449d3634f7d77fb8a73f
robhyrkiel <rob.hyrkiel@gmail.com>
2019-08-29 15:59:57 -0400

    feat: added notes CPT for subscriber users

--- themes/fultonU/functions.php
+++ themes/fultonU/functions.php
@@ -219,6 +219,21 @@ function uni_post_types() {
         ),
         'menu_icon' => 'dashicons-welcome-learn-more'
     ));
+    // Note Post Type
+    register_post_type('note', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor', 'author'),
+        'public' => false,
+        'show_ui' => true,
+        'labels' => array(
+            'name' => 'Notes',
+            'add_new_item' => 'Add New Note',
+            'edit_item' => 'Edit Note',
+            'all_items' => 'All Notes',
+            'singular_name' => 'Note'
+        ),
+        'menu_icon' => 'dashicons-welcome-write-blog'
+    ));
 }
 
 add_action('init', 'uni_post_types');
